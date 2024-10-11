4e443c8ff0b1b9edb06fd8c45ffc6f953ed11e69
nikolay <n.ilin@it-park.tech>
2024-10-11 16:26:26 +0300

    hw 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,5 @@ from catalog import views
 urlpatterns = {
     path('', views.home, name='home'),
     path('contacts/', views.contact, name='contact'),
+    path('product/<int:product_id>', views.product, name='product'),
 }
